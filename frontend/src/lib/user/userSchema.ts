65b5388379ec7d0ad5ac3af4792f30dfab390768
MarkRWolf <igotzeddm@gmail.com>
2025-12-18 16:13:53 +0100

    fixes obsolete zod ts typing

--- frontend/src/lib/user/userSchema.ts
+++ frontend/src/lib/user/userSchema.ts
@@ -2,7 +2,7 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
-  email: z.string().email("Invalid email address"),
+  email: z.email("Invalid email address"),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters")
@@ -12,7 +12,7 @@ export const signupSchema = z.object({
 });
 
 export const loginSchema = z.object({
-  email: z.string().email("Invalid email address"),
+  email: z.email("Invalid email address"),
   password: z.string().nonempty("Password is required"),
 });
 
